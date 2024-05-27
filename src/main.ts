6b7a17a17b4aed56cf7b6ea8a897e7d3059c69b6
Dadi Atar <dadi@montara.io>
2024-05-27 10:10:40 +0300

    before publishing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
+import axios from 'axios'
+
+type PipelineRunStatus =
+  | 'pending'
+  | 'in_progress'
+  | 'failed'
+  | 'completed'
+  | 'conflict'
 
 /**
  * The main function for the action.
@@ -7,18 +15,33 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const webhookUrl: string = core.getInput('webhookUrl')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Triggerring Montara pipeline with webhookUrl: ${webhookUrl}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const webhookResponse = await axios.post<{
+      runId: string
+      webhookId: string
+    }>(webhookUrl)
+    core.debug(`Response: ${JSON.stringify(webhookResponse)}`)
+    const counter = 0
+    while (counter < 10) {
+      const runStatus = await axios.get<{
+        id: string
+        status: PipelineRunStatus
+      }>(
+        `https://hooks.montara.io/pipeline/run/status?runId=${webhookResponse?.data?.runId}`
+      )
+      if (runStatus.data.status === 'completed') {
+        core.setOutput('isPassing', true)
+        break
+      } else if (runStatus.data.status === 'failed') {
+        core.setOutput('isPassing', false)
+        break
+      }
+      await wait(1000)
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
