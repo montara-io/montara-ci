52c3ef90306430763bb76b2e2da9626e45cf56b8
Dadi Atar <dadi@montara.io>
2024-05-27 16:48:35 +0300

    test

--- dist/index.js
+++ dist/index.js
@@ -32701,7 +32701,7 @@ exports.PIPELINE_RUN_STATUS = void 0;
 exports.PIPELINE_RUN_STATUS = `
 # Montara CI report
 
-:{{status_icon}} pipeline finished with status {{status}}
+:{{status_icon}}: pipeline finished with status {{status}}
 
 [View run in Montara](https://{{montara_prefix}}.montara.io/app/pipelines/{{pipeline_id}}openModalRunId={{run_id}})
 
--- src/comment-templates.ts
+++ src/comment-templates.ts
@@ -1,7 +1,7 @@
 export const PIPELINE_RUN_STATUS = `
 # Montara CI report
 
-:{{status_icon}} pipeline finished with status {{status}}
+:{{status_icon}}: pipeline finished with status {{status}}
 
 [View run in Montara](https://{{montara_prefix}}.montara.io/app/pipelines/{{pipeline_id}}openModalRunId={{run_id}})
 
