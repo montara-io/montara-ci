9abecf56497c66a2eace81e1e0f19d3b60bfe2ee
Dadi Atar <dadi@montara.io>
2024-05-28 15:13:54 +0300

    log context

--- src/github.ts
+++ src/github.ts
@@ -8,6 +8,7 @@ export async function postComment({
   const octokit = github.getOctokit(process.env.MONTARA_GITHUB_TOKEN ?? '')
   const context = github.context
   const { pull_request, repository } = context.payload
+  console.log('context payload', context.payload)
 
   if (!pull_request) {
     console.log('No pull request found in the context')
